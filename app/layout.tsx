54b7a1c5eab6ee3fb158977328e36e3c8775592a
Jorge Sardon <anibal317@gmail.com>
2024-11-23 18:14:11 -0300

    sitio base

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Whatsapp from "@/components/Whatsapp/Whatsapp";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+      <Whatsapp phoneNumber="542314401066" positionX="30px" positionY="90px"/>
       </body>
     </html>
   );
---
+++ components/Whatsapp/Whatsapp.tsx
@@ -0,0 +1,21 @@
+import './Whatsapp.css'
+import Link from 'next/link';
+
+interface WhatsappProps {
+    phoneNumber: string;
+    positionX: string;
+    positionY: string;
+}
+
+export default function Whatsapp({ phoneNumber, positionX, positionY }: WhatsappProps) {
+    return (
+        <Link href={`https://wa.me/${phoneNumber}?`} style={{ bottom: positionY, right: positionX }} className="whatsapp" target="_blank" rel="noopener noreferrer">
+            <img
+                src="/whatsapp.png"
+                alt="Whatsapp Turismo Tapalqué"
+                className='h-16'
+            />
+            {/* <FaSquareWhatsapp style={{ backgroundColor: 'white', borderRadius: '5px', padding: '1px' }} fill='green' size={'3em'} /> */}
+        </Link>
+    )
+}
