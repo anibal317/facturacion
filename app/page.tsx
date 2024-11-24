e437f87bff63425a936041ce68fa0dd49fdeb5b2
Jorge Sardon <anibal317@gmail.com>
2024-11-24 17:45:59 -0300

    agregando caracteristicas al home

--- app/page.tsx
+++ app/page.tsx
@@ -24,12 +24,27 @@ export default function Home() {
                   Facturación Simple
                 </h1>
                 <p className="mx-auto max-w-[700px] text-gray-500 md:text-xl dark:text-gray-400">
-                  Simplifica tu proceso de facturación con nuestro plugin intuitivo y potente.
+                  Simplifica tu proceso de facturación con nuestro sistema intuitivo y potente.
                 </p>
               </div>
+              <Button>Prueba Gratis</Button>
               <div className="space-x-4">
-                <Button>Prueba Gratis</Button>
-                <Button variant="outline">Más Información</Button>
+                <article className="flex items-start text-left">
+                  <ul className=" list-disc">
+                    <h1 className="text-[2rem]">Principales caracteristicas:</h1>
+                    <li className="">Guardá clientes y productos sin límites.</li>
+                    <li className="">Cree fácil facturas digitales, electrónicas</li>
+                    <li className="">Verifica el stock en tiempo real</li>
+                    <li className="">Lleva el cuenta corriente de cliente y provedores</li>
+                    <li className="">Aumenta el precio de tus productos de forma masiva</li>
+                    <li className="">Genere libro de iva rapidamente</li>
+                    <li className="">Revise estadisticas se su Empresa</li>
+                    <li className="">Gestion de cajas</li>
+                    <li className="">Importacion y exportacion de productos y clientes</li>
+                    <li className="">Genere presupuestos</li>
+                    <li className="">Maneje deferentes listas de precios</li>
+                  </ul>
+                </article>
               </div>
             </div>
           </div>
