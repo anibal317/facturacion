3ab9083d888e0522c76a1177b11a008392fcdfd1
Jorge Sardon <anibal317@gmail.com>
2024-12-03 17:21:27 -0300

    fixing prisma files to allow render on netlify

--- app/api/plans/route.ts
+++ app/api/plans/route.ts
@@ -57,3 +57,9 @@
 //         );
 //     }
 // }
+
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request, { params }: { params: { id: string } }) {
+  return NextResponse.json({ message: `Feature ID: ${params.id}` });
+}
