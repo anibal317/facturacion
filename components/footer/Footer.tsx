f11f0d262973e192c8c741477afce1880b4c71ae
Jorge Sardon <anibal317@gmail.com>
2024-12-09 18:43:10 -0300

    making nav and footer responsive

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,9 +1,15 @@
+'use client'
+
 import Link from "next/link";
+import Image from "next/image";
 import data from "@/data/featureNavigation.json"
 import { NavLink } from '../../types/navigation'
+import { useState } from 'react';
 
 export default function Footer() {
     const enabledMainLinks = data.mainLinks.filter(link => link.enabled !== false)
+    const [isExploreOpen, setIsExploreOpen] = useState(false);
+
     const renderNavLink = (link: NavLink & { enabled?: boolean }) => {
         if (link.enabled === false) return null;
         return (
@@ -16,43 +22,51 @@ export default function Footer() {
             </Link>
         )
     }
-    return <footer className="flex sm:flex-row flex-col justify-center items-center bg-[#758395] mt-12 px-4 md:px-6 py-2 border-t text-[#fff]">
-        <div className="flex flex-col bg-[#374151] p-2 w-screen text-[#e5e7eb]">
-            <div className="flex flex-row">
-                <div className="flex flex-col justify-center w-[35%] h-[35%]">
-                    <div className="flex items-center gap-4 w-full">
-                        <img alt="Logo Preview" src="../../favicon.ico" width="90" />
-                        <div className="text-5xl">Bolívar SOFTWARE</div>
+
+    const toggleExplore = () => {
+        setIsExploreOpen(!isExploreOpen);
+    }
+
+    return (
+        <footer className="bg-[#374151] mt-12 text-[#e5e7eb]">
+            <div className="mx-auto px-4 py-8 container">
+                <div className="flex lg:flex-row flex-col justify-between items-center lg:items-start">
+                    <div className="mb-8 lg:mb-0 text-center lg:text-left">
+                        <div className="flex lg:flex-row flex-col items-center lg:items-start gap-4 mb-4">
+                            <Image src="/favicon.ico" alt="Logo Preview" width={90} height={90} />
+                            <div className="mt-2 lg:mt-0 text-3xl lg:text-5xl">Bolívar SOFTWARE</div>
+                        </div>
+                        <div className="flex justify-center lg:justify-start gap-6 mt-4">
+                            <a href="#" aria-label="Twitter"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 1200 1227"><path fill="#fff" d="M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"/></svg></a>
+                            <a href="#" aria-label="YouTube"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a>
+                            <a href="#" aria-label="Facebook"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
+                            <a href="#" aria-label="Instagram"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" preserveAspectRatio="xMidYMid" viewBox="0 0 256 256"><path fill="#fff" d="M128 23.064c34.177 0 38.225.13 51.722.745 12.48.57 19.258 2.655 23.769 4.408 5.974 2.322 10.238 5.096 14.717 9.575 4.48 4.479 7.253 8.743 9.575 14.717 1.753 4.511 3.838 11.289 4.408 23.768.615 13.498.745 17.546.745 51.723 0 34.178-.13 38.226-.745 51.723-.57 12.48-2.655 19.257-4.408 23.768-2.322 5.974-5.096 10.239-9.575 14.718-4.479 4.479-8.743 7.253-14.717 9.574-4.511 1.753-11.289 3.839-23.769 4.408-13.495.616-17.543.746-51.722.746-34.18 0-38.228-.13-51.723-.746-12.48-.57-19.257-2.655-23.768-4.408-5.974-2.321-10.239-5.095-14.718-9.574-4.479-4.48-7.253-8.744-9.574-14.718-1.753-4.51-3.839-11.288-4.408-23.768-.616-13.497-.746-17.545-.746-51.723 0-34.177.13-38.225.746-51.722.57-12.48 2.655-19.258 4.408-23.769 2.321-5.974 5.095-10.238 9.574-14.717 4.48-4.48 8.744-7.253 14.718-9.575 4.51-1.753 11.288-3.838 23.768-4.408 13.497-.615 17.545-.745 51.723-.745M128 0C93.237 0 88.878.147 75.226.77c-13.625.622-22.93 2.786-31.071 5.95-8.418 3.271-15.556 7.648-22.672 14.764C14.367 28.6 9.991 35.738 6.72 44.155 3.555 52.297 1.392 61.602.77 75.226.147 88.878 0 93.237 0 128c0 34.763.147 39.122.77 52.774.622 13.625 2.785 22.93 5.95 31.071 3.27 8.417 7.647 15.556 14.763 22.672 7.116 7.116 14.254 11.492 22.672 14.763 8.142 3.165 17.446 5.328 31.07 5.95 13.653.623 18.012.77 52.775.77s39.122-.147 52.774-.77c13.624-.622 22.929-2.785 31.07-5.95 8.418-3.27 15.556-7.647 22.672-14.763 7.116-7.116 11.493-14.254 14.764-22.672 3.164-8.142 5.328-17.446 5.95-31.07.623-13.653.77-18.012.77-52.775s-.147-39.122-.77-52.774c-.622-13.624-2.786-22.929-5.95-31.07-3.271-8.418-7.648-15.556-14.764-22.672C227.4 14.368 220.262 9.99 211.845 6.72c-8.142-3.164-17.447-5.328-31.071-5.95C167.122.147 162.763 0 128 0Zm0 62.27C91.698 62.27 62.27 91.7 62.27 128c0 36.302 29.428 65.73 65.73 65.73 36.301 0 65.73-29.428 65.73-65.73 0-36.301-29.429-65.73-65.73-65.73Zm0 108.397c-23.564 0-42.667-19.103-42.667-42.667S104.436 85.333 128 85.333s42.667 19.103 42.667 42.667-19.103 42.667-42.667 42.667Zm83.686-110.994c0 8.484-6.876 15.36-15.36 15.36-8.483 0-15.36-6.876-15.36-15.36 0-8.483 6.877-15.36 15.36-15.36 8.484 0 15.36 6.877 15.36 15.36Z"/></svg></a>
+                        </div>
                     </div>
-                    <div className="gap-6 grid grid-cols-4 mx-auto p-4">
-                        <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 1200 1227"><path fill="#fff" d="M714.163 519.284 1160.89 0h-105.86L667.137 450.887 357.328 0H0l468.492 681.821L0 1226.37h105.866l409.625-476.152 327.181 476.152H1200L714.137 519.284h.026ZM569.165 687.828l-47.468-67.894-377.686-540.24h162.604l304.797 435.991 47.468 67.894 396.2 566.721H892.476L569.165 687.854v-.026Z"/></svg></a> 
-                         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path></svg></a> 
-                         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
-                         <a href="#"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" preserveAspectRatio="xMidYMid" viewBox="0 0 256 256"><path fill="#fff" d="M128 23.064c34.177 0 38.225.13 51.722.745 12.48.57 19.258 2.655 23.769 4.408 5.974 2.322 10.238 5.096 14.717 9.575 4.48 4.479 7.253 8.743 9.575 14.717 1.753 4.511 3.838 11.289 4.408 23.768.615 13.498.745 17.546.745 51.723 0 34.178-.13 38.226-.745 51.723-.57 12.48-2.655 19.257-4.408 23.768-2.322 5.974-5.096 10.239-9.575 14.718-4.479 4.479-8.743 7.253-14.717 9.574-4.511 1.753-11.289 3.839-23.769 4.408-13.495.616-17.543.746-51.722.746-34.18 0-38.228-.13-51.723-.746-12.48-.57-19.257-2.655-23.768-4.408-5.974-2.321-10.239-5.095-14.718-9.574-4.479-4.48-7.253-8.744-9.574-14.718-1.753-4.51-3.839-11.288-4.408-23.768-.616-13.497-.746-17.545-.746-51.723 0-34.177.13-38.225.746-51.722.57-12.48 2.655-19.258 4.408-23.769 2.321-5.974 5.095-10.238 9.574-14.717 4.48-4.48 8.744-7.253 14.718-9.575 4.51-1.753 11.288-3.838 23.768-4.408 13.497-.615 17.545-.745 51.723-.745M128 0C93.237 0 88.878.147 75.226.77c-13.625.622-22.93 2.786-31.071 5.95-8.418 3.271-15.556 7.648-22.672 14.764C14.367 28.6 9.991 35.738 6.72 44.155 3.555 52.297 1.392 61.602.77 75.226.147 88.878 0 93.237 0 128c0 34.763.147 39.122.77 52.774.622 13.625 2.785 22.93 5.95 31.071 3.27 8.417 7.647 15.556 14.763 22.672 7.116 7.116 14.254 11.492 22.672 14.763 8.142 3.165 17.446 5.328 31.07 5.95 13.653.623 18.012.77 52.775.77s39.122-.147 52.774-.77c13.624-.622 22.929-2.785 31.07-5.95 8.418-3.27 15.556-7.647 22.672-14.763 7.116-7.116 11.493-14.254 14.764-22.672 3.164-8.142 5.328-17.446 5.95-31.07.623-13.653.77-18.012.77-52.775s-.147-39.122-.77-52.774c-.622-13.624-2.786-22.929-5.95-31.07-3.271-8.418-7.648-15.556-14.764-22.672C227.4 14.368 220.262 9.99 211.845 6.72c-8.142-3.164-17.447-5.328-31.071-5.95C167.122.147 162.763 0 128 0Zm0 62.27C91.698 62.27 62.27 91.7 62.27 128c0 36.302 29.428 65.73 65.73 65.73 36.301 0 65.73-29.428 65.73-65.73 0-36.301-29.429-65.73-65.73-65.73Zm0 108.397c-23.564 0-42.667-19.103-42.667-42.667S104.436 85.333 128 85.333s42.667 19.103 42.667 42.667-19.103 42.667-42.667 42.667Zm83.686-110.994c0 8.484-6.876 15.36-15.36 15.36-8.483 0-15.36-6.876-15.36-15.36 0-8.483 6.877-15.36 15.36-15.36 8.484 0 15.36 6.877 15.36 15.36Z"/></svg></a>                    
+                    <div className="flex-col w-full lg:w-auto">
+                        <button 
+                            onClick={toggleExplore}
+                            className="flex justify-between items-center lg:hidden bg-[#4B5563] mb-4 px-4 py-2 rounded w-full text-white"
+                        >
+                            <span>Descubre</span>
+                            <svg className={`w-4 h-4 transition-transform ${isExploreOpen ? 'transform rotate-180' : ''}`} fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+                            </svg>
+                        </button>
+                        <div className={`${isExploreOpen ? 'block' : 'hidden'} lg:block`}>
+                            <h3 className="mb-4 font-bold text-[#9ca3af] text-lg uppercase">Secciones</h3>
+                            <div className="flex flex-col gap-2">
+                                {enabledMainLinks.map(renderNavLink)}
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="flex flex-row justify-end gap-16 w-4/5 text-nowrap">
-                    <div className="gap-6 grid grid-cols-3">
-                        <div className="flex flex-col">
-                            <div className="pb-3 font-bold text-[#9ca3af] uppercase">Explore</div>
-                            {enabledMainLinks.map(renderNavLink)}
-                            {/* <a href="#xxx" className="">Features</a>
-                            <a href="#xxx" className="hover:underline">Docs</a>
-                            <a href="#xxx" className="hover:underline">Pricing</a>
-                            <a href="#xxx" className="hover:underline">Security</a> */}
-                        </div>
-                        {/* <div className="flex flex-col gap-2">
-                        </div>
-                        <div className="flex flex-col gap-2">
-                        </div> */}
-                    </div>
-                    {/* <div className="flex flex-col gap-2">
-                    </div> */}
+                <div className="border-gray-500 my-8 border-t"></div>
+                <div className="text-center">
+                    © 2024 Bolivar Software. Todos los derechos reservados.
                 </div>
             </div>
-            <div className="border-gray-500 m-2 border-t"></div>
-            <div className="text-center">© 2024 Bolivar Software. Todos los derechos reservados.</div>
+        </footer>
+    )
+}
 
-        </div>
-    </footer>
-};
--- components/navBar/Navigation.tsx
+++ components/navBar/Navigation.tsx
@@ -3,6 +3,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { Menu, X } from 'lucide-react'
 
 interface Logo {
   icon: string
@@ -26,6 +27,7 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ variant }) => {
   const [navData, setNavData] = useState<NavData | null>(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
 
   useEffect(() => {
     const fetchNavData = async () => {
@@ -43,29 +45,53 @@ const Navbar: React.FC<NavbarProps> = ({ variant }) => {
   if (!navData) return null
 
   const navStyles = {
-    home: 'fixed flex items-center bg-[#4e637c] px-4 lg:px-6 w-screen h-14 text-[#fff]',
-    feature: 'fixed flex items-center bg-[#4e637c] px-4 lg:px-6 w-screen h-14 text-[#fff]',
+    home: 'fixed bg-[#4e637c] w-full text-[#fff] z-50',
+    feature: 'fixed bg-[#4e637c] w-full text-[#fff] z-50',
+  }
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen)
   }
 
   return (
-    <nav className={`p-4 ${navStyles[variant]}`}>
-      <div className="mx-auto container">
-        <div className="flex justify-between items-center">
+    <nav className={`${navStyles[variant]}`}>
+      <div className="mx-auto px-4 lg:px-6 container">
+        <div className="flex justify-between items-center h-14">
           <Link href={navData.logo.href} className="flex items-center">
-            <Image src={navData.logo.icon} alt="Logo" width={40} height={40} />
-            <span className="ml-2 font-bold text-2xl">{navData.logo.text}</span>
+            <Image src={navData.logo.icon} alt="Logo" width={32} height={32} className="w-8 h-8" />
+            <span className="ml-2 font-bold text-lg sm:text-xl lg:text-2xl">{navData.logo.text}</span>
           </Link>
-          <ul className="flex space-x-4">
+          {/* Cambiamos el breakpoint de 'md' a 'lg' para evitar solapamiento en 768px */}
+          <div className="lg:flex space-x-4 hidden">
             {navData.links.map((link) => (
-              <li key={link.href}>
-                <Link href={link.href} className="hover:underline">
-                  {link.text}
-                </Link>
-              </li>
+              <Link key={link.href} href={link.href} className="text-sm lg:text-base hover:underline">
+                {link.text}
+              </Link>
             ))}
-          </ul>
+          </div>
+          {/* Ajustamos el botón del menú para que aparezca hasta 1023px */}
+          <button className="lg:hidden" onClick={toggleMenu} aria-label="Toggle menu">
+            {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+          </button>
         </div>
       </div>
+      {/* Menú móvil: visible hasta 1023px */}
+      {isMenuOpen && (
+        <div className="lg:hidden">
+          <div className="space-y-1 px-2 pt-2 pb-3">
+            {navData.links.map((link) => (
+              <Link
+                key={link.href}
+                href={link.href}
+                className="block hover:bg-[#5a7290] px-3 py-2 rounded-md font-medium text-sm hover:text-white"
+                onClick={toggleMenu}
+              >
+                {link.text}
+              </Link>
+            ))}
+          </div>
+        </div>
+      )}
     </nav>
   )
 }
