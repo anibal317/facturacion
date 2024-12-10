c4ffa8e0a1b1a857626320da2b9bfd7640d17af0
Jorge Sardon <anibal317@gmail.com>
2024-12-10 20:58:41 -0300

    fixing faqs

--- app/products/qa/page.tsx
+++ app/products/qa/page.tsx
@@ -1,5 +1,3 @@
-// app/products/qa/page.tsx
-
 import Layout from "@/components/layout/Layout";
 import {
     Accordion,
@@ -7,22 +5,29 @@ import {
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion";
-import { getFAQs } from '@/app/products/qa/services/faqService' // Asegúrate de que la ruta sea correcta
-import { FAQ } from '@/types/faq'; // Asegúrate de que la ruta sea correcta
+import faqs from "../../../data/faqs.json"; // Asegúrate de usar la ruta correcta
 
-// Componente de página
-const Page = async () => {
-    const faqs: FAQ[] = await getFAQs(); // Llama a la función del servicio
+// Define el tipo de las propiedades
+interface CustomAccordionContentProps {
+    htmlContent: string;
+}
 
+const CustomAccordionContent: React.FC<CustomAccordionContentProps> = ({ htmlContent }) => {
+    return (
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    );
+};
+
+export default function Page() {
     return (
         <Layout variant='feature'>
             <div className="flex justify-center items-center pt-16 pb-16 divide-y divide-dashed">
                 <Accordion type="single" collapsible className="w-3/4">
                     {faqs.map((faq) => (
-                        <AccordionItem key={faq.id} value={faq.id.toString()}>
+                        <AccordionItem key={faq.value} value={faq.value}>
                             <AccordionTrigger>{faq.question}</AccordionTrigger>
                             <AccordionContent>
-                                <div dangerouslySetInnerHTML={{ __html: faq.answer }} />
+                                <CustomAccordionContent htmlContent={faq.answer} />
                             </AccordionContent>
                         </AccordionItem>
                     ))}
@@ -30,6 +35,4 @@ const Page = async () => {
             </div>
         </Layout>
     );
-};
-
-export default Page;
+}
