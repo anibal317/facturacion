8d58cdd15819978f03efdc630b5545ab0778bce8
Jorge Sardon <anibal317@gmail.com>
2024-12-10 20:50:36 -0300

    finxing faqs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export',
+//     distDir: 'dist',
+//     images: { unoptimized: true }
+// };
+
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
 const nextConfig = {
     output: 'export',
     distDir: 'dist',
-    images: { unoptimized: true }
+    images: { unoptimized: true },
 };
 
 export default nextConfig;
