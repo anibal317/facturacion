4027ec499b51a62d337db694f587f9f9ca49e6dd
Jorge Sardon <anibal317@gmail.com>
2024-12-17 20:22:21 -0300

    fixing faqs

--- app/products/qa/page.tsx
+++ app/products/qa/page.tsx
@@ -1,24 +1,49 @@
+'use client'
 import Layout from "@/components/layout/Layout";
+import Spinner from "@/components/spinner/Spinner";
 import {
     Accordion,
     AccordionContent,
     AccordionItem,
     AccordionTrigger,
 } from "@/components/ui/accordion";
-import faqs from "../../../data/faqs.json"; // Asegúrate de usar la ruta correcta
+import { useEffect, useState } from "react";
 
 // Define el tipo de las propiedades
-interface CustomAccordionContentProps {
-    htmlContent: string;
+interface FAQ {
+    id: number;
+    value: string;
+    question: string;
+    answer: string;
 }
 
-const CustomAccordionContent: React.FC<CustomAccordionContentProps> = ({ htmlContent }) => {
-    return (
-        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-    );
-};
-
 export default function Page() {
+    const [faqs, setFaqs] = useState<FAQ[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchFAQs = async () => {
+            try {
+                const response = await fetch("https://facturacion-admin.onrender.com/api/faqs");
+                if (!response.ok) {
+                    throw new Error("Error fetching FAQs");
+                }
+                const data = await response.json();
+                setFaqs(data); // Asumiendo que el endpoint devuelve un array de FAQs
+            } catch (error:any) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchFAQs();
+    }, []);
+
+    if (loading) return (<div className="flex justify-center items-center w-screen h-full"><Spinner/></div>);
+    if (error) return <div>Error: {error}</div>;
+
     return (
         <Layout variant='feature'>
             <div className="flex justify-center items-center pt-16 pb-16 divide-y divide-dashed">
@@ -27,7 +52,7 @@ export default function Page() {
                         <AccordionItem key={faq.value} value={faq.value}>
                             <AccordionTrigger>{faq.question}</AccordionTrigger>
                             <AccordionContent>
-                                <CustomAccordionContent htmlContent={faq.answer} />
+                                <div dangerouslySetInnerHTML={{ __html: faq.answer }} />
                             </AccordionContent>
                         </AccordionItem>
                     ))}
